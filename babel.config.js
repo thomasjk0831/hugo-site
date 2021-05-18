776539d8236499bcebd855081b9af1d862fc73ea
Thomas Kim <tkim@nexient.com>
2021-05-18 15:13:08 -0400

    added preact

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,33 @@
-module.exports = function (api) {
+// module.exports = function (api) {
+//     api.cache(true);
+//     const presets = [['@babel/preset-react']];
+//     const plugins = [];
+//     return {
+//       presets,
+//       plugins,
+//     };
+//   };
+
+  module.exports = function (api) {
     api.cache(true);
-    const presets = [['@babel/preset-react']];
-    const plugins = [];
+    const presets = [['@babel/preset-env']];
+    const plugins = [
+        ["@babel/plugin-transform-react-jsx", {
+            "pragma": "h",
+            "pragmaFrag": "Fragment",
+          }]
+    ];
     return {
       presets,
       plugins,
     };
   };
+
+// {
+//     "plugins": [
+//       ["@babel/plugin-transform-react-jsx", {
+//         "pragma": "h",
+//         "pragmaFrag": "Fragment",
+//       }]
+//     ]
+//   }
